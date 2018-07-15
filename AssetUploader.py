b2ce18126f474fe67e643df61ff50d427c9f1d8f
blair <bhagen55@gmail.com>
2018-07-15 16:50:46 -0700

    Renaming

--- AssetUploader.py
+++ AssetUploader.py
@@ -1,11 +1,13 @@
 import sys
 from backend import BoxInterface
+from treeitems import BoxTreeModel, BoxTreeItem
 from PyQt5.QtCore import (QDate, QDateTime, QRegExp, QSortFilterProxyModel, Qt,
         QTime)
 from PyQt5.QtGui import QIcon, QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QGridLayout,
         QGroupBox, QHBoxLayout, QLabel, QLineEdit, QTreeView, QVBoxLayout,
         QWidget)
+
 
 
 class AssetUploader(QWidget):
@@ -35,33 +37,21 @@ class AssetUploader(QWidget):
         # Tree view
         self.dataGroupBox = QGroupBox("Asset Store")
         self.dataView = QTreeView()
-        self.dataView.setRootIsDecorated(False)
-        self.dataView.setAlternatingRowColors(True)
+        #self.dataView.setRootIsDecorated(False)
+        #self.dataView.setAlternatingRowColors(True)
 
         dataLayout = QHBoxLayout()
         dataLayout.addWidget(self.dataView)
         self.dataGroupBox.setLayout(dataLayout)
 
-        model = self.createBoxFileModel(self)
+        model = BoxTreeModel.BoxTreeModel(self.box)
         self.dataView.setModel(model)
-
-        for file in self.box.get_folder_contents(self.box.get_folder('0')):
-            self.addFile(model, file)
 
         mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.dataGroupBox)
         self.setLayout(mainLayout)
 
         self.show()
-
-    def createBoxFileModel(self, parent):
-        model = QStandardItemModel(0, 1, parent)
-        model.setHeaderData(0, Qt.Horizontal, "Name")
-        return model
-
-    def addFile(self, model, file):
-        model.insertRow(0, QStandardItem(file['name']))
-
 
 
 if __name__ == '__main__':
