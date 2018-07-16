e47bbdc2a3fb59b467af4e6d491c6b12499dc73a
Blair Hagen <hagenb@union.edu>
2018-07-16 12:40:51 -0700

    Redoing boxitem class

--- AssetUploader.py
+++ AssetUploader.py
@@ -1,13 +1,10 @@
 import sys
 from backend import BoxInterface
-from treeitems import BoxTreeModel, BoxTreeItem
-from PyQt5.QtCore import (QDate, QDateTime, QRegExp, QSortFilterProxyModel, Qt,
-        QTime)
+from pathlib import Path
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QGridLayout,
-        QGroupBox, QHBoxLayout, QLabel, QLineEdit, QTreeView, QVBoxLayout,
-        QWidget)
-
+from PyQt5.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QTreeView,
+        QVBoxLayout, QWidget, QMenu, QMessageBox)
 
 
 class AssetUploader(QWidget):
@@ -18,6 +15,9 @@ class AssetUploader(QWidget):
         # Setup Box interface
         self.box = BoxInterface.BoxInterface("./keys/appauth.json")
 
+        # Icon location
+        self.iconPath = Path("./icons")
+
         self.title = ("Asset Uploader")
 
         self.left = 10
@@ -27,6 +27,7 @@ class AssetUploader(QWidget):
 
         self.initUI()
 
+
     def initUI(self):
         # Title
         self.setWindowTitle(self.title)
@@ -34,24 +35,78 @@ class AssetUploader(QWidget):
         # Location/size
         self.setGeometry(self.left, self.top, self.width, self.height)
 
+        # Icons
+        self.folderIcon = QIcon(str(self.iconPath) + "/folder.png")
+
         # Tree view
-        self.dataGroupBox = QGroupBox("Asset Store")
-        self.dataView = QTreeView()
-        #self.dataView.setRootIsDecorated(False)
-        #self.dataView.setAlternatingRowColors(True)
-
+        self.treeGroupBox = QGroupBox("Asset Store")
+        self.treeView = QTreeView()
+        self.treeView.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.treeView.customContextMenuRequested.connect(self.contextMenu)
         dataLayout = QHBoxLayout()
-        dataLayout.addWidget(self.dataView)
-        self.dataGroupBox.setLayout(dataLayout)
+        dataLayout.addWidget(self.treeView)
+        self.treeGroupBox.setLayout(dataLayout)
 
-        model = BoxTreeModel.BoxTreeModel(self.box)
-        self.dataView.setModel(model)
+        # Box root
+        self.root = QStandardItem()
+        self.root.setData(self.box.get_folder('0'))
+        self.root.setText("Root")
+        self.root.setIcon(self.folderIcon)
 
+        # Setup file tree
+        model = QStandardItemModel()
+        model.appendRow(self.root)
+        self.treeView.setModel(model)
+        self.buildFileTree(self.root)
+
+        # Add all to main layout
         mainLayout = QVBoxLayout()
-        mainLayout.addWidget(self.dataGroupBox)
+        mainLayout.addWidget(self.treeGroupBox)
         self.setLayout(mainLayout)
 
         self.show()
+
+
+    def buildFileTree(self, root):
+        for item in self.box.get_folder_contents(root.data()):
+            new = QStandardItem(item['name'])
+            new.setData(item)
+            root.appendRow(new)
+            print("Adding " + new.data()['name'] + " to " + root.data()['name'])
+            if item['type'] == "folder":
+                    new.setIcon(self.folderIcon)
+                    self.buildFileTree(new)
+
+
+    def contextMenu(self, position):
+        index = self.treeView.selectedIndexes()[0]
+        selected = index.model().itemFromIndex(index)
+        boxData = selected.data()
+        itemType = boxData['type']
+
+        menu = QMenu()
+        rename = menu.addAction("Rename")
+        delete = menu.addAction("Delete")
+        if itemType == 'folder':
+            upload = menu.addAction("Upload Here")
+        elif itemType == 'file':
+            link = menu.addAction("Get Link")
+
+        action = menu.exec_(self.treeView.viewport().mapToGlobal(position))
+
+        if itemType == 'file' and action == link:
+            linkWindow = QMessageBox()
+            linkWindow.setWindowTitle("Permalink")
+            linkWindow.setText(self.box.get_direct_download(boxData))
+            linkWindow.setTextInteractionFlags(Qt.TextSelectableByMouse)
+            linkWindow.exec()
+        elif action == rename:
+
+            print("rename")
+        elif action == delete:
+            print("delete")
+        elif action == upload:
+            print("upload")
 
 
 if __name__ == '__main__':
